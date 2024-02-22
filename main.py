5a2c4fb923619e645289489043c8f8ad7764eac1
Abtin <71178014+Abtin0@users.noreply.github.com>
2024-02-21 20:08:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+
+print("Hello and welcome to the robux generator!")
+a = input("Please enter your roblox username:\n")
+print("Authenticating Username...")
+time.sleep(2)
+print("Username found!")
+time.sleep(.5)
+b = int(input("How much robux do you want?\n"))
+print("Generating Robux...")
+time.sleep(5)
+print(f'{b} robux added to account!')
